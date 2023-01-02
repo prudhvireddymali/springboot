0e6f33b095c9138a624df449f5e57eedf5928232
prudhvireddymali <prudhvireddymali2021@gmail.com>
2023-01-02 17:13:41 -0600

    spring Boot

---
+++ springbootone/src/main/java/com/mali/springbootone/CourseController.java
@@ -0,0 +1,20 @@
+package com.mali.springbootone;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CourseController {
+	@RequestMapping("/courses")
+	public List<Course> retrieveAllCourses(){
+		return Arrays.asList(
+				new Course(1,"aws", "w3school"),
+				new Course(1,"aws", "w3school")
+				);
+	}
+	
+
+}
